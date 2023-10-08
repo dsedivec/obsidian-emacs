1621a6657351e711bed23e21927990c65f1e834d
Dale Sedivec <dale@codefu.org>
2023-10-07 21:11:38 -0500

    Very minimal use of Emacs bindings in CM6

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Obsidian Show White Space Plug-In
+# Obsidian Emacs Bindings
 
-This is a plugin for Obsidian (https://obsidian.md).  It uses
-CodeMirror 6's built-in `highlightWhitespace` plug-in to visualize
-white space.
+This is a plugin for Obsidian (https://obsidian.md).  It uses [this
+generously provided
+extension](https://github.com/replit/codemirror-emacs) to add Emacs
+key bindings to Obsidian's editor.
 
 
 # License
--- main.ts
+++ main.ts
@@ -1,80 +1,20 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { highlightWhitespace } from '@codemirror/view';
+import { Plugin } from 'obsidian';
+import { Extension } from '@codemirror/state';
+import { emacs } from "@replit/codemirror-emacs"
 
-interface ShowWhiteSpaceSettings {
-    showWhiteSpace: boolean;
-}
-
-const DEFAULT_SETTINGS: ShowWhiteSpaceSettings = {
-    showWhiteSpace: true,
-}
-
-export default class ShowWhiteSpace extends Plugin {
-    settings: ShowWhiteSpaceSettings;
-    editorExtensions: Array;
-
-    async onSettingsChanged() {
-        const extensionRegistered = (this.editorExtensions.length > 0);
-        if (this.settings.showWhiteSpace != extensionRegistered) {
-            if (this.settings.showWhiteSpace) {
-                this.editorExtensions.push(highlightWhitespace());
-            } else {
-                this.editorExtensions.splice(0, this.editorExtensions.length);
-            }
-            this.app.workspace.updateOptions();
-        }
-    }
+export default class ObsidianEmacsKeymap extends Plugin {
+    // We use this to hold an array we *could* mutate to
+    // activate/deactivate this extension.  That isn't used here right
+    // now, but it could be.
+    editorExtensions: Array<Extension>;
 
     async onload() {
-	await this.loadSettings();
-        this.editorExtensions = [];
+        this.editorExtensions = [emacs()];
         this.registerEditorExtension(this.editorExtensions);
-        await this.onSettingsChanged();
-	this.addSettingTab(new ShowWhiteSpaceSettingsTab(this.app, this));
     }
 
     async onunload() {
         // I don't actually think this is necessary, but just in case...
         this.editorExtensions.splice(0, this.editorExtensions.length);
-    }
-
-    async loadSettings() {
-	this.settings = Object.assign(
-            {},
-            DEFAULT_SETTINGS,
-            await this.loadData(),
-        );
-    }
-
-    async saveSettings() {
-	await this.saveData(this.settings);
-        await this.onSettingsChanged();
-    }
-}
-
-class ShowWhiteSpaceSettingsTab extends PluginSettingTab {
-    plugin: ShowWhiteSpace;
-
-    constructor(app: App, plugin: ShowWhiteSpace) {
-	super(app, plugin);
-	this.plugin = plugin;
-    }
-
-    display(): void {
-	const {containerEl} = this;
-
-	containerEl.empty();
-
-	new Setting(containerEl)
-	    .setName('Show white space in editor')
-	    .setDesc('Visualize spaces and tabs in the editor.')
-            .addToggle(
-                toggle => toggle
-                    .setValue(this.plugin.settings.showWhiteSpace)
-                    .onChange(async (value) => {
-		        this.plugin.settings.showWhiteSpace = value;
-		        await this.plugin.saveSettings();
-	            })
-            );
     }
 }
